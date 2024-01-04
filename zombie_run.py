f8c110d672a4e55a915104544d398b6e17418857
Michaël Cloots <michael.cloots@thomasmore.be>
2024-01-04 17:28:24 +0100

    first commit

---
+++ zombie_run.py
@@ -0,0 +1,12 @@
+# Install pgzero (Settings - Project - Interpreter)
+import pgzrun
+import random
+from pgzhelper import *
+
+# Game screen dimension
+TITLE = 'Zombie Runner'
+WIDTH = 800
+HEIGHT = 600
+
+# Run the game
+pgzrun.go()
