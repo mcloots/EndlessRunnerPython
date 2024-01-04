7a94735966a1f3092ae0966e3a4f8fddbcb9e63e
Michaël Cloots <michael.cloots@thomasmore.be>
2024-01-04 18:38:07 +0100

    moon

--- zombie_run.py
+++ zombie_run.py
@@ -8,5 +8,23 @@ TITLE = 'Zombie Runner'
 WIDTH = 800
 HEIGHT = 600
 
+# Colours
+black = (0, 0, 0)
+brown = (71, 34, 18)
+
+# Moon
+moon = Actor('moon')
+moon.x = WIDTH - 100
+moon.y = 80
+
+
+# Rect: 0,0 = x, y
+def draw():
+    screen.draw.filled_rect(Rect(0, 0, WIDTH, HEIGHT - 100), (black))  # Sky
+    screen.draw.filled_rect(Rect(0, 500, WIDTH, HEIGHT), (brown))  # Ground
+
+    moon.draw()
+
+
 # Run the game
 pgzrun.go()
